71b52560bb116847a719aa3b3dc81ebaf90540d4
Armando <armando.laplaca@gmail.com>
2021-07-01 16:46:00 +0200

    created project template

---
+++ src/main/java/it/unipa/bigdata/dmi/lda/LdaLncRnaDiseaseAssociationApplication.java
@@ -0,0 +1,13 @@
+package it.unipa.bigdata.dmi.lda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdaLncRnaDiseaseAssociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdaLncRnaDiseaseAssociationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unipa/bigdata/dmi/lda/LdaLncRnaDiseaseAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipa.bigdata.dmi.lda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdaLncRnaDiseaseAssociationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
