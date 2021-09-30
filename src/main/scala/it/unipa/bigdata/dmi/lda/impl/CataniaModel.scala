4f184ccf8259f552b2573237e6ba859ce8a2fd7d
Armando <armando.laplaca@gmail.com>
2021-09-30 18:26:36 +0200

    add support to CataniaModel
    Add support to create the matrices used by ncPred model and load of ncPred output

--- src/main/scala/it/unipa/bigdata/dmi/lda/impl/CataniaModel.scala
+++ src/main/scala/it/unipa/bigdata/dmi/lda/impl/CataniaModel.scala
@@ -1,52 +1,84 @@
 package it.unipa.bigdata.dmi.lda.impl
 
-import java.io.{File, FileOutputStream, PrintWriter}
+import java.io.{FileOutputStream, PrintWriter}
 
+import it.unipa.bigdata.dmi.lda.builder.PredictionBuilder
 import it.unipa.bigdata.dmi.lda.config.LDACli
 import it.unipa.bigdata.dmi.lda.factory.LoggerFactory
 import it.unipa.bigdata.dmi.lda.model.{Prediction, PredictionFDR}
+import it.unipa.bigdata.dmi.lda.utility.FDRFunction
 import org.apache.commons.lang.NotImplementedException
 import org.apache.log4j.Logger
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.DoubleType
 
 import scala.collection.mutable.WrappedArray
 
 class CataniaModel() extends GraphframeAbstractModel() {
   private val logger: Logger = LoggerFactory.getLogger(classOf[CataniaModel])
 
-  override def loadPredictions(): Dataset[PredictionFDR] = {
-    if (predictions == null) {
-      val tmp = sparkSession.read.parquet(s"resources/predictions/${LDACli.getVersion}/catania_fdr/").withColumn("gs", when(col("gs").equalTo(1.0), true).otherwise(false).as("gs"))
-      val names = classOf[PredictionFDR].getDeclaredFields.union(classOf[PredictionFDR].getSuperclass.getDeclaredFields).map(f => f.getName)
-      val mapColumn: Column = map(tmp.drop(names: _*).columns.tail.flatMap(name => Seq(lit(name), col(s"$name"))): _*)
-      predictions = tmp.withColumn("parameters", mapColumn).select("parameters", names: _*)
-        .as[PredictionFDR](Encoders.bean(classOf[PredictionFDR])).cache()
+  private def loadFromFile(): Dataset[Prediction] = {
+    import sparkSession.implicits._
+    val predictions_raw = sparkSession.sparkContext.textFile(LDACli.getScoresPath)
+    val header = predictions_raw.first.toUpperCase
+    val predictions_DF = predictions_raw.filter(line => !line.equals(header)).map(v => v.toUpperCase.split("\t")).toDF()
+    val diseases = header.split("\t").map(v => v.replace("\"", ""))
+
+    val extractLncrnaUDF = udf((arr: WrappedArray[String]) => arr(0).replace("\"", ""))
+    val assignDiseaseUDF = udf((arr: WrappedArray[String]) => {
+      val values = arr.slice(1, arr.size)
+      for (i <- 0 to values.size - 1)
+        values.update(i, diseases(i) + "=" + values(i))
+      values
     }
-    logger.info(s"Caching Catania predictions: ${predictions.count()}")
+    )
+    val extractDiseaseUDF = udf((combo: String) => combo.split("=")(0))
+    val extractValueUDF = udf((combo: String) => combo.split("=")(1))
 
-    predictions
-  }
+    val extracted_predictions = predictions_DF
+      .withColumn("lncrna", extractLncrnaUDF(col("value")))
+      .withColumn("diseaseValues", assignDiseaseUDF(col("value")))
+      .drop("value")
+      .withColumn("diseaseValue", explode(col("diseaseValues")))
+      .drop("diseaseValues")
+      .withColumn("disease", extractDiseaseUDF(col("diseaseValue")))
+      .withColumn("score", extractValueUDF(col("diseaseValue")))
+      .drop("diseaseValue")
+      .sort(col("score").desc)
+      .coalesce(1)
+      .withColumn("rank", monotonically_increasing_id())
 
-  override def auc(): BinaryClassificationMetrics = {
-    if (predictions == null) {
-      logger.info("AUC: loading predictions")
-      predictions = loadPredictions()
-    }
-    logger.info("AUC: computing")
-    auc(predictions.withColumn("fdr", lit(1) - col("fdr") as "fdr")
-      .as[PredictionFDR](Encoders.bean(classOf[PredictionFDR])))
-  }
-
-  override def confusionMatrix(): DataFrame = {
-    val scores = loadPredictions()
-      .select(col("prediction"), when(col("gs").equalTo(1.0), true).otherwise(false).as("gs"))
-      .groupBy("gs", "prediction").agg(count("gs").as("count"))
-      .sort(col("gs").desc, col("prediction").desc)
-    println("------------\nCatania Confusion Matrix")
-    scores.show(false)
-    println("------------")
+    val lncRNA_disease_DF = datasetReader.getLncrnaDisease()
+    scores = sparkSession.createDataset(
+      extracted_predictions.join(
+        broadcast(lncRNA_disease_DF.withColumn("value", lit(1)).na.fill(0, Seq("value"))).as("lncRNA_disease_DF"),
+        lncRNA_disease_DF("lncrna").equalTo(extracted_predictions("lncrna")).and(lncRNA_disease_DF("disease").equalTo(extracted_predictions("disease"))),
+        "fullOuter"
+      )
+        .select(col("rank"), extracted_predictions("lncrna"), extracted_predictions("disease"), extracted_predictions("score").as("score").cast(DoubleType), col("value").as("gs").cast(DoubleType))
+        .na.fill(0.0, Seq("gs"))
+        .na.fill(0.0, Seq("score"))
+        .rdd.map(row => {
+        val lnc = row.getString(row.fieldIndex("lncrna"))
+        val disease = row.getString(row.fieldIndex("disease"))
+        val score = row.getDouble(row.fieldIndex("score"))
+        val gs = row.getDouble(row.fieldIndex("gs"))
+        val rank = row.getLong(row.fieldIndex("rank"))
+        new PredictionBuilder()
+          .setLncrna(lnc)
+          .setDisease(disease)
+          .setScore(score)
+          .setGs(if (gs == 0.0) false else true)
+          .setParameter("rank", rank.toString)
+          .build()
+      }))(Encoders.bean(classOf[Prediction]))
+      .repartition(360)
+      .cache()
+    logger.info(s"Caching scores ${scores.count()}")
+    saveResults(scores
+      .select("lncrna", "disease", "score", "gs"))
     scores
   }
 
@@ -96,22 +128,77 @@ class CataniaModel() extends GraphframeAbstractModel() {
 
   }
 
+  override def loadPredictions(): Dataset[PredictionFDR] = {
+    if (predictions == null) {
+      val tmp = sparkSession.read.parquet(s"resources/predictions/${LDACli.getVersion}/catania_fdr/").withColumn("gs", when(col("gs").equalTo(1.0), true).otherwise(false).as("gs"))
+      val names = classOf[PredictionFDR].getDeclaredFields.union(classOf[PredictionFDR].getSuperclass.getDeclaredFields).map(f => f.getName)
+      val mapColumn: Column = map(tmp.drop(names: _*).columns.tail.flatMap(name => Seq(lit(name), col(s"$name"))): _*)
+      predictions = tmp.withColumn("parameters", mapColumn).select("parameters", names: _*)
+        .as[PredictionFDR](Encoders.bean(classOf[PredictionFDR])).cache()
+    }
+    logger.info(s"Caching Catania predictions: ${predictions.count()}")
+    predictions
+  }
+
+  override def auc(): BinaryClassificationMetrics = {
+    if (predictions == null) {
+      logger.info("AUC: loading predictions")
+      predictions = loadPredictions()
+    }
+    logger.info("AUC: computing")
+    auc(predictions.withColumn("fdr", lit(1) - col("fdr") as "fdr")
+      .as[PredictionFDR](Encoders.bean(classOf[PredictionFDR])))
+  }
+
+  override def confusionMatrix(): DataFrame = {
+    val scores = loadPredictions()
+      .select(col("prediction"), when(col("gs").equalTo(1.0), true).otherwise(false).as("gs"))
+      .groupBy("gs", "prediction").agg(count("gs").as("count"))
+      .sort(col("gs").desc, col("prediction").desc)
+    println("------------\nCatania Confusion Matrix")
+    scores.show(false)
+    println("------------")
+    scores
+  }
+
   override def compute(): Dataset[Prediction] = {
     createMatrix()
     throw new NotImplementedException("This application doesn't implement the prediction functionality of nPred model. It is possible to create the matrix file to be submitted for the execution of the model.")
   }
 
   override def predict(): Dataset[PredictionFDR] = {
-    createMatrix()
-    throw new NotImplementedException("This application doesn't implement the prediction functionality of nPred model. It is possible to create the matrix file to be submitted for the execution of the model.")
+    val scores = loadScores()
+    predictions = FDRFunction().computeFDR(scores)
+    logger.info("catania predictions")
+    predictions.show(false)
+    saveResults(predictions
+      .select("lncrna", "disease", "score", "fdr", "rank", "gs", "prediction"))
+    predictions
   }
 
   override def loadScores(): Dataset[Prediction] = {
-    if (scores == null)
-      scores = sparkSession.read.parquet(s"resources/predictions/${LDACli.getVersion}/catania/")
-        .withColumnRenamed("PValue", "score").as[Prediction](Encoders.bean(classOf[Prediction]))
+    if (LDACli.getScoresPath != null) {
+      scores = loadFromFile()
+    }
+    else if (scores == null)
+      scores = sparkSession.createDataset(
+        sparkSession.read.parquet(s"resources/predictions/${LDACli.getVersion}/catania/")
+          .withColumnRenamed("PValue", "score")
+          .rdd.map(row => {
+          val lnc = row.getString(row.fieldIndex("lncrna"))
+          val disease = row.getString(row.fieldIndex("disease"))
+          val score = row.getDouble(row.fieldIndex("score"))
+          val gs = row.getDouble(row.fieldIndex("gs"))
+          new PredictionBuilder()
+            .setLncrna(lnc)
+            .setDisease(disease)
+            .setScore(score)
+            .setGs(if (gs == 0.0) false else true)
+            .build()
+        }))(Encoders.bean(classOf[Prediction]))
         .cache()
     logger.info(s"Caching Catania scores: ${scores.count()}")
+    scores.show(false)
     scores
   }
 }
