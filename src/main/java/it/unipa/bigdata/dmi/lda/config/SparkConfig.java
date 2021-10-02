f4ceb278fd7b4a484a8a96d832fcf64b2dbd0d4c
Armando <armando.laplaca@gmail.com>
2021-10-02 16:04:02 +0200

    Add spark.properties file
    - SparkConfig now uses the properties file to set the spark configuration. If any errors occurs, it goes with default values.

--- src/main/java/it/unipa/bigdata/dmi/lda/config/SparkConfig.java
+++ src/main/java/it/unipa/bigdata/dmi/lda/config/SparkConfig.java
@@ -1,22 +1,46 @@
 package it.unipa.bigdata.dmi.lda.config;
 
+import it.unipa.bigdata.dmi.lda.factory.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class SparkConfig {
+    private static Logger logger = LoggerFactory.getLogger(SparkConfig.class);
     private String appName = "LDA";
 
     private String masterUri = "local[*]";
 
-    private String driverMemory = "6G";
-
-    private String driverCores = "6";
+    private Map<String, String> otherProperties = new HashMap<>();
 
     public SparkConf conf() {
-        return new SparkConf()
+        try {
+            loadProperties();
+        } catch (IOException e) {
+            logger.warn("Cannot load properties, going with defaults");
+        }
+        SparkConf sc = new SparkConf()
                 .setAppName(appName)
-                .setMaster(masterUri)
-                .set("spark.driver.memory", driverMemory)
-                .set("spark.driver.cores", driverCores);
+                .setMaster(masterUri);
+        otherProperties.forEach(sc::set);
+        return sc;
+    }
+
+    private void loadProperties() throws IOException {
+        Properties properties = new Properties();
+        properties.load(new InputStreamReader(new FileInputStream("src/main/resources/spark.properties")));
+        appName = properties.getProperty("appName", appName);
+        masterUri = properties.getProperty("masterUri", masterUri);
+        properties.keySet().stream()
+                .filter(key -> !"appName".equals(key) && !"masterUri".equals(key))
+                .map(Object::toString)
+                .forEach(key -> otherProperties.put(key, properties.getProperty(key)));
     }
 
     public String getAppName() {
@@ -27,5 +51,9 @@ public class SparkConfig {
         return masterUri;
     }
 
+    public static void main(String[] args) {
+        SparkConfig sc = new SparkConfig();
+        sc.conf();
+    }
 
 }
